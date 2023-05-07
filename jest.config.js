acf920a4892cab1facee53bc2e0a7a5469f20676
Joep <joep@mindhash.nl>
2023-05-07 14:16:20 +0200

    Removed coverage requirments

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
-  coverageThreshold:{
-    branches: 100,
-    functions: 100,
-    lines: 100,
-    statements: 100
-  },
   randomize: true,
   coveragePathIgnorePatterns : [
     "<rootDir>/src/helpers/constants.ts",
